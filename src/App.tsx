6604a565f24fd095ad43bbdf9c91c7278d36f8e6
doker696 <doker696@users.noreply.github.com>
2021-07-12 12:52:46 +0300

    created form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Dispatch } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+
+import { addTimeTrack, removeTimeTrack } from "./store/actionCreators";
+import { TimeTrackHistory } from "./components/TimeTrackHistory";
+import { TimeTracker } from "./components/TimeTracker";
+import { Container, createStyles, makeStyles, Theme } from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: '4px 8px',
+      margin: "150px auto",
+      display: 'flex',
+      alignItems: 'center',
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  }),
+);
 
 function App() {
+  const classes = useStyles();
+  const timeTracks: readonly ITimeObj[] = useSelector(
+    (state: TimeTracksState) => state.tracks
+  );
+
+  const dispatch: Dispatch<any> = useDispatch();
+
+  const saveTimeTrack = React.useCallback(
+    (timeTrack: ITimeObj) => dispatch(addTimeTrack(timeTrack)),
+    [dispatch]
+  );
+  const deleteTimeTrack = React.useCallback(
+    (timeTrack: ITimeObj) => dispatch(removeTimeTrack(timeTrack)),
+    [dispatch]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Container maxWidth="md">
+        <TimeTracker
+          _save={saveTimeTrack}  
+        ></TimeTracker>
+        <TimeTrackHistory
+          _timeTracks={timeTracks}
+          _delete={deleteTimeTrack}
+        ></TimeTrackHistory>
+      </Container>
     </div>
   );
 }
