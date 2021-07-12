82d063a59d56a6fe3f1572ceb32712d7fd398a39
doker696 <doker696@users.noreply.github.com>
2021-07-12 12:50:02 +0300

    added redux store and localStorage support

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware, Store } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import App from "./App";
+import reducer from "./store/reducer";
 
-ReactDOM.render(
-  <React.StrictMode>
+function saveToLocalStorage(state: TimeTracksState) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+
+const store: Store<TimeTracksState, TimeAction> & {
+  dispatch: DispatchType;
+} = createStore(reducer, loadFromLocalStorage(), applyMiddleware(thunk));
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+const rootElement = document.getElementById("root");
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
