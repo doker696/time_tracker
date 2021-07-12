85b6599fb00d825df6a89283c97d8faf12eab0cc
doker696 <doker696@users.noreply.github.com>
2021-07-12 12:51:45 +0300

    created timer

---
+++ src/components/Timer.tsx
@@ -0,0 +1,52 @@
+import { createStyles, IconButton, makeStyles, Theme, Typography } from "@material-ui/core";
+import { PlayArrow, Stop } from "@material-ui/icons";
+
+import React from "react";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+        margin: "auto"
+    }
+  })
+);
+
+export function Timer(props: { time: number }) {
+  const classes = useStyles();
+
+  return (
+    <Typography variant="subtitle1" className={classes.root}>
+      <span className="digits">
+        {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
+      </span>
+      <span className="digits">
+        {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
+      </span>
+      <span className="digits mili-sec">
+        {("0" + ((props.time / 10) % 100)).slice(-2)}
+      </span>
+    </Typography>
+  );
+}
+export function ControlButton(props: {
+  handleStart: Function;
+  handleStop: Function;
+  active: Boolean;
+}) {
+  const StartButton = (
+    <IconButton onClick={() => props.handleStart()}>
+      <PlayArrow />
+    </IconButton>
+  );
+  const StopButton = (
+    <IconButton onClick={() => props.handleStop()}>
+      <Stop />
+    </IconButton>
+  );
+
+  return (
+    <div className="Control-Buttons">
+      <div>{props.active ? StopButton : StartButton}</div>
+    </div>
+  );
+}
